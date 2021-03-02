c247a7f4d14e15d971ff4732c065b0bd2e6e721d
andru <andru2400@hotmail.com>
2021-03-02 13:16:24 -0500

    first commit

---
+++ resources/js/Pages/Groups/Index.vue
@@ -0,0 +1,36 @@
+// <template>
+    <app-layout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Módulo de Grupos
+            </h2>
+        </template>
+
+        <div class="py-12">
+            Hola
+        </div>
+    </app-layout>
+</template>
+
+<script>
+    import AppLayout from '@/Layouts/AppLayout'
+
+    export default {
+        components: {
+            AppLayout,
+        },
+        props: {
+            groups: Array,
+        },
+        data() {
+            return {
+                // q: ''
+            }
+        },
+        // watch: {
+        //     q: function (value) {
+        //         this.$inertia.replace(this.route('notes.index', {q: value}))
+        //     }
+        // }
+    }
+</script>
